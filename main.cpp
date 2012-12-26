3dd62a2e0dbd8f6f5a987bb90d19d15c3a418c62
HP Elitebook <prog-hp1@core2062.com>
2012-12-26 11:11:20 -0600

    Wrote code for subsystem framework.

--- main.cpp
+++ main.cpp
@@ -1,136 +1,87 @@
 #include "WPILib.h"
 #include "util.h"
+#include <string>
+#include <stdio.h>
 
+#include "DriveSubsystem.h"
+#include "TurretSubsystem.h"
+#include "LiftSubsystem.h"
+#include "SweepSubsystem.h"
+#include "BridgeSubsystem.h"
+#define SSNUM 5
 
 class RobotDemo : public SimpleRobot
 {
-	// Joysticks
 	Joystick joystick1;
 	Joystick joystick2;
 	
-	CANJaguar turret;
-	Victor bridgeManipulatorMotor;
-	Servo bridgeLock;
+	DriveSubsystem drive;
+	TurretSubsystem turret;
+	LiftSubsystem lift;
+	SweepSubsystem sweep;
+	BridgeSubsystem bridge;
 	
-	// Lift Motors
-	Victor liftMotorFront;
-	Victor liftMotorBack;
-	
-	// Sweeper Motors
-	Victor sweepMotorLeft;
-	Victor sweepMotorRight;
-	
-	// Shooter Motors
-	CANJaguar shooterTop;
-	CANJaguar shooterBottom;
-	
-	// Turret Limit Switches
-	DigitalInput turretLimitLeft;
-	DigitalInput turretLimitRight;
-	
-	// Bridge Limit Switches
-	DigitalInput bridgeSwitchTop;
-	DigitalInput bridgeSwitchBtm;
-	
-	// Thumbwheel Digits
-	DigitalInput thumb1;
-	DigitalInput thumb2;
-	DigitalInput thumb3;
-	
-	DriverStation * ds;
-	
+	CoreSubsystem subsystems [SSNUM];
 public:
 	RobotDemo(void):
 		joystick1(1),
 		joystick2(2),
-				
-		turret(17),
-		bridgeManipulatorMotor(4),
-		bridgeLock(9),
-		
-		liftMotorFront(1),
-		liftMotorBack(2),
-		
-		sweepMotorLeft(3),
-		sweepMotorRight(5),
-		
-		shooterTop(11),
-		shooterBottom(15),
-		
-		turretLimitLeft(6),
-		turretLimitRight(7),
-		
-		bridgeSwitchTop(4),
-		bridgeSwitchBtm(5),
-		
-		thumb1(9),
-		thumb2(10),
-		thumb3(11)
+		drive(),
+		turret(),
+		lift(),
+		sweep(),
+		bridge()
 	{
-		
-		shooterTop.SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
-		shooterBottom.SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
-		
-		shooterTop.ConfigEncoderCodesPerRev(360);
-		shooterBottom.ConfigEncoderCodesPerRev(360);
-		
-		shooterTop.SetPID(1.5,.025,.005);
-		shooterBottom.SetPID(1.5,.025,.005);
-		ds = DriverStation::GetInstance();
+		subsystems[0] = drive;
+		subsystems[1] = turret;
+		subsystems[2] = lift;
+		subsystems[3] = sweep;
+		subsystems[4] = bridge;
 	}
 
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
 	void Autonomous(void)
 	{
-//		myRobot.SetSafetyEnabled(false);
-//		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-//		Wait(2.0); 				//    for 2 seconds
-//		myRobot.Drive(0.0, 0.0); 	// stop robot
+		//Nope.avi
+		Wait(.005);
 	}
 
 	void OperatorControl(void)
 	{
-		Timer bTimer;
-		bTimer.Start();
+		Watchdog &wd = GetWatchdog();
+		wd.SetEnabled(true);
+		wd.SetExpiration(1);
 		
-		bool bridgeButtonOld = false;
-		bool shooterOverride = false;
+		for (int i = 0; i < SSNUM; ++i) {
+			subsystems[i].teleop_init();
+			if(check_and_break("init ", i)){return;}
+			wd.Feed();
+		}
 		
 		while(IsOperatorControl() and !IsDisabled()){
-			
-			// Bridge Manipuator code
-			bool bridgeButton = joystick1.GetRawButton(5);
-			float bridgeOutput = 0;
-			
-			if (bridgeButton){
-				bridgeOutput = bridgeSwitchBtm.Get() ? 0 : -0.5;
-			} else {
-				bridgeOutput = bridgeSwitchTop.Get() ? 0 : 0.5;
+			for (int i = 0; i < SSNUM; ++i){
+				subsystems[i].teleop_joystick(joystick1,joystick2);
+				if(check_and_break("joystick ", i)){return;}
+				wd.Feed();
 			}
-
-			if	((ds->GetMatchTime() > 15.5) and (bTimer.Get() >= 0.5)){
-				bridgeManipulatorMotor.SetSpeed(bridgeOutput);
+			for (int i = 0; i < SSNUM; ++i){
+				subsystems[i].teleop_main();
+				if(check_and_break("main ", i)){return;}
+				wd.Feed();
 			}
-			bridgeLock.SetAngle((bridgeButton and bridgeSwitchBtm.Get()) ? 180 : 80);
-			
-			if (bridgeButtonOld != bridgeButton){
-				bTimer.Reset();
+			for (int i = 0; i < SSNUM; ++i){
+				subsystems[i].teleop_motors();
+				if(check_and_break("motors ", i)){return;}
+				wd.Feed();
 			}
-			bridgeButtonOld = bridgeButton;
-			
-			//Shooter Code
-			if(joystick2.GetRawButton(4)){
-				shooterOverride = true;
-			}
-			
-			if(joystick2.GetRawButton(6)){
-				shooterOverride = false;
-			}
-			
-			float throttle = joystick2.GetRawAxis(6);
 		}
+		
+	}
+	bool check_and_break(std::string string, int num){
+		if ( !( IsOperatorControl() and !IsDisabled() ) ){
+			cout << string << num;
+			return true;
+		}
+		return false;
 	}
 };
 
