5e594d32e170c9fc97705bfd4cd7f41aa149c637
HP Elitebook <prog-hp1@core2062.com>
2013-01-03 14:45:27 -0600

    Moved CoreSubsystem implementation to header.

--- CoreSubsystem.h
+++ CoreSubsystem.h
@@ -8,17 +8,27 @@ class CORESubsystem{
 	public:
 	
 	std::string name;
-	CORESubsystem(void);
+	CORESubsystem(void){
+		name = "undefined name";
+	}
 	
 	virtual ~CORESubsystem(void){};	// Suppresses GNU GCC warning. Can be removed under GCC version 4.3
 	
 	// Called before loop at start of Teleop period
-	virtual void teleop_init(void);
+	virtual void teleop_init(void){
+		printf("Unimplemented teleop_init\n");
+	}
 	
 	//Called sequentially during loop, interleaved with other subsystems
-	virtual void teleop_joystick(COREJoystick& joystick);
-	virtual void teleop_main(void);
-	virtual void teleop_motors(void);
+	virtual void teleop_joystick(COREJoystick& joystick){
+		printf("Unimplemented teleop_joystick\n");
+	}
+	virtual void teleop_main(void){
+		printf("Unimplemented teleop_main\n");
+	}
+	virtual void teleop_motors(void){
+		printf("Unimplemented teleop_joystick\n");
+	}
 };
 
 #endif
